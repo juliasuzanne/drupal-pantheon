6c6551796f9a35bf4724873d6151f056d7fe103d
Julia Grimes <julia.s.grimes@gmail.com>
2024-06-15 00:08:06 -0400

    Get rid of discrepancy

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,41 +7,38 @@
 /**
  *
  *
- * @type {import('gatsby').GatsbyNode['createPages']}
  */
 
-if (typeof window !== `undefined`) {
-  const path = require("path")
+const path = require("path")
 
-  exports.createPages = async ({ actions, graphql }) => {
-    const { createPage } = actions
-    // createPage({
-    //   path: "/using-dsg",
-    //   component: require.resolve("./src/templates/using-dsg.js"),
-    //   context: {},
-    //   defer: true,
-    // })
-  }
-  const articles = await graphql(`
-    {
-      allNodeArticle {
-        nodes {
-          id
-          title
-          path {
-            alias
-          }
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions
+  // createPage({
+  //   path: "/using-dsg",
+  //   component: require.resolve("./src/templates/using-dsg.js"),
+  //   context: {},
+  //   defer: true,
+  // })
+}
+const articles = await graphql(`
+  {
+    allNodeArticle {
+      nodes {
+        id
+        title
+        path {
+          alias
         }
       }
     }
-  `)
+  }
+`)
 
-  articles.data.allNodeArticle.nodes.map(articleData =>
-    createPage({
-      path: articleData.path.alias,
-      component: path.resolve(`src/templates/article.js`),
-      context: { ArticleId: articleData.id },
-      // defer: true,
-    })
-  )
-}
+articles.data.allNodeArticle.nodes.map(articleData =>
+  createPage({
+    path: articleData.path.alias,
+    component: path.resolve(`src/templates/article.js`),
+    context: { ArticleId: articleData.id },
+    // defer: true,
+  })
+)
