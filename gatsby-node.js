f5eaa867c923314bada3cf725bea2691133b2473
Julia Grimes <julia.s.grimes@gmail.com>
2024-06-14 23:38:20 -0400

    Troubleshooting window issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,18 +10,18 @@
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
 
-// if (typeof window !== `undefined`) {
-const path = require("path")
-// }
-exports.createPages = async ({ actions, graphql }) => {
-  const { createPage } = actions
-  // createPage({
-  //   path: "/using-dsg",
-  //   component: require.resolve("./src/templates/using-dsg.js"),
-  //   context: {},
-  //   defer: true,
-  // })
+if (typeof window !== `undefined`) {
+  const path = require("path")
 
+  exports.createPages = async ({ actions, graphql }) => {
+    const { createPage } = actions
+    // createPage({
+    //   path: "/using-dsg",
+    //   component: require.resolve("./src/templates/using-dsg.js"),
+    //   context: {},
+    //   defer: true,
+    // })
+  }
   const articles = await graphql(`
     {
       allNodeArticle {
